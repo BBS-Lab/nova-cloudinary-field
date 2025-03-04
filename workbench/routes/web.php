2c0755fe2321b32f870e57f058caf0b4471b1e6c
Mikaël Popowicz <mikael.popowicz@big-boss-studio.com>
2025-03-04 11:39:32 +0100

    Add Nova 5 support

--- workbench/routes/web.php
+++ workbench/routes/web.php
@@ -7,7 +7,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('debug', function () {
-    return config('nova-cloudinary');
-});
